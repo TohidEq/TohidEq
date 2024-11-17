1426f711db795f7fbc56ca677e86a023b125c24f
Tohid Eghdami <TohidEq@gmail.com>
2024-11-17 23:55:22 +0330

    Config: Add raw Nvim (LazyVim)

---
+++ .dotfiles/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+  "justinsgithub/oh-my-monokai.nvim",
+  lazy = true,
+  priority = 1000,
+  config = function()
+    require("oh-my-monokai").setup({
+      transparent_background = true,
+      terminal_colors = true,
+      devicons = true, -- highlight the icons of `nvim-web-devicons`
+      palette = "default", -- or create your own ^^ e.g. justinsgithub
+      inc_search = "underline", -- underline | background
+      background_clear = {
+        "float_win",
+        "toggleterm",
+        "telescope",
+        "which-key",
+        "renamer",
+      }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree"
+      plugins = {
+        bufferline = {
+          underline_selected = false,
+          underline_visible = false,
+        },
+        indent_blankline = {
+          context_start_underline = false,
+        },
+      },
+      ---@param c Colorscheme
+      override = function(c) end,
+    })
+  end,
+}
